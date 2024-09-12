fbb6d563985749f395d58f1f111ceaeb5b65f87e
mrinal1224 <iammrinalkc@gmail.com>
2024-09-12 09:30:21 +0530

    push

---
+++ Class-10(Introduction to JS)/script.js
@@ -0,0 +1,25 @@
+// console.log('hello from SST')
+
+// var a = 12.67890
+// console.log(a)
+
+// var b= 'scaler'
+// console.log(b)
+
+
+// var c = true
+// console.log(c)
+
+
+// var a = 'Ferrari' // Redeclartion
+// console.log(a)
+
+// a = false // Reassignment
+
+// console.log(a)
+
+let a = 'Scaler'
+
+let a = 12345
+
+console.log(a)
