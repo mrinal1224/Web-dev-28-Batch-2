48d72e357dc380fe2982d89c11f59e508c35b3cb
mrinal1224 <iammrinalkc@gmail.com>
2024-09-17 09:28:02 +0530

    push

--- Class-11 (Arrays , Objects and Strings)/array.js
+++ Class-11 (Arrays , Objects and Strings)/array.js
@@ -1,14 +1,42 @@
 // Arrays 
 // an orderd collection of data - index
 
-let evenNums = [2 , 4, 6 , 8 , 10 , 12]
+// let evenNums = [2 , 'scaler', true , 'a' , 10 , 12]
 
-console.log(evenNums)
+// console.log(evenNums)
 
-let multipliedArr = []
+// let multipliedArr = []
 
-for(let i=0 ; i<evenNums.length ; i++ ){
-      multipliedArr.push(evenNums[i]*2)
-}
+// for(let i=0 ; i<evenNums.length ; i++ ){
+//       multipliedArr.push(evenNums[i]*2)
+// }
 
-console.log(multipliedArr)
+// console.log(multipliedArr)
+
+
+
+
+let cars = ['BMW' , 'Porshe' , 'Ferrari' , 'Mercedes']
+
+// Push
+
+cars.push('bentley')
+
+console.log(cars)
+
+// pop
+
+const firstDeletedCar = cars.pop()
+console.log(firstDeletedCar)
+
+console.log(cars)
+
+// unshift
+
+cars.unshift('Urus')
+console.log(cars)
+
+// shift
+
+cars.shift()
+console.log(cars)
