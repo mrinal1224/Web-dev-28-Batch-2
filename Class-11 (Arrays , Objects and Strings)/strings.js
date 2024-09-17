9727985ded1504be2808a2c3a9f0c7b3cc2a6f04
mrinal1224 <iammrinalkc@gmail.com>
2024-09-17 10:03:27 +0530

    push

---
+++ Class-11 (Arrays , Objects and Strings)/strings.js
@@ -0,0 +1,55 @@
+//  String is a sequnce of Characters
+
+let str = 'Scaler'
+
+console.log(str.length)
+
+// String methods
+
+// Extraction Methods
+
+// Slice
+
+let slicedStr = str.slice(2 , 4)
+
+console.log(slicedStr)
+
+
+// Replace method
+
+let text1 = 'Hello World'
+let text2 = 'Goodbye World'
+
+let text3 = text1.replace("Hello" , 'Namaste' )
+
+console.log(text3)
+
+
+// toLowerCase
+
+let text = 'SCALER'
+
+let lowerCaseStr = text.toLowerCase()
+console.log(lowerCaseStr)
+
+
+let text4 = 'ferrari'
+
+let upperCaseStr = text4.toUpperCase()
+console.log(upperCaseStr)
+
+// Concatenation
+
+let greeting = "hello"
+let personName = 'Tanishq'
+
+let completeGreeting = greeting.concat(" "+ personName)
+
+console.log(completeGreeting)
+
+
+
+
+
+
+
