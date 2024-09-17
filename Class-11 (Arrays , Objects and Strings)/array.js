d7302b4b9cd6fedf78d7c9716e33cabd4e2d9f6b
mrinal1224 <iammrinalkc@gmail.com>
2024-09-17 09:05:44 +0530

    push

---
+++ Class-11 (Arrays , Objects and Strings)/array.js
@@ -0,0 +1,14 @@
+// Arrays 
+// an orderd collection of data - index
+
+let evenNums = [2 , 4, 6 , 8 , 10 , 12]
+
+console.log(evenNums)
+
+let multipliedArr = []
+
+for(let i=0 ; i<evenNums.length ; i++ ){
+      multipliedArr.push(evenNums[i]*2)
+}
+
+console.log(multipliedArr)
