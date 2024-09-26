57234b86d9ed488be1310941b0d33c3bd992f324
mrinal1224 <iammrinalkc@gmail.com>
2024-09-26 09:20:46 +0530

    push

---
+++ Class-12(Execution Context)/exercise.js
@@ -0,0 +1,10 @@
+var n = 3
+
+function sqaure(num){
+   var ans = num*num
+   return ans
+}
+
+
+var sqaure1 = sqaure(n) // 9
+var sqaure2 = sqaure(4)// 16
--- Class-12(Execution Context)/script.js
+++ Class-12(Execution Context)/script.js
@@ -1,16 +1,20 @@
-var a = 10
+var a=10;
+
 
 
 var b = 12
 
 
-function addTwoNumber(m ,n){
-    return m+n
+function add(m ,n){
+   var sum = m+n
+   return sum
 }
 
-const sum1 = addTwoNumber(a , b)
-const sum2 = addTwoNumber(4,5)
 
 
-console.log(sum1)
-console.log(sum2)
+const sum1 = add(a , b)
+const sum2 = add(4,5)
+
+
+console.log(sum1) // 22
+console.log(sum2) // 9
