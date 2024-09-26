1978861e1b18132e509b50b2e0f7b5574bc5ec0f
mrinal1224 <iammrinalkc@gmail.com>
2024-09-26 08:53:53 +0530

    push

---
+++ Class-12(Execution Context)/script.js
@@ -0,0 +1,16 @@
+var a = 10
+
+
+var b = 12
+
+
+function addTwoNumber(m ,n){
+    return m+n
+}
+
+const sum1 = addTwoNumber(a , b)
+const sum2 = addTwoNumber(4,5)
+
+
+console.log(sum1)
+console.log(sum2)
