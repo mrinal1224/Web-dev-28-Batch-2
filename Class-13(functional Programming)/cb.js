3280d836ca3a7c5a9c6d50fc7c5899f7963c3512
mrinal1224 <iammrinalkc@gmail.com>
2024-10-01 09:04:27 +0530

    push

---
+++ Class-13(functional Programming)/cb.js
@@ -0,0 +1,18 @@
+function firstName(name , cb){
+    console.log(name)
+    cb('Singh')
+}
+
+
+
+function lastName(lastName){
+    console.log(lastName)
+}
+
+function printAge(age){
+    console.log(age)
+}
+
+
+firstName('Tanishq' , lastName)
+
